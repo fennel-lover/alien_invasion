a1d0949522bd051b6575f24c25080f3b830680f9
fennle-lover <futon6@163.com>
2023-10-03 12:14:40 +0800

    12.4添加飞船图像

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,16 +2,24 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """管理游戏资源和行为的类"""
 
     def __init__(self):
         """初始化游戏并创建游戏资源。"""
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        # 赋给属性self.screen 的对象是一个surface。
+        # 在Pygame中，surface是屏幕的一部分，用于显示游戏元素。
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                                self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-    
+
+        self.ship = Ship(self)
+
     def run_game(self):
         """开始游戏的主循环"""
         while True:
@@ -19,6 +27,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            #每次循环时都重绘屏幕。
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # 让最近绘制的屏幕可见。
             pygame.display.flip()
